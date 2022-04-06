15dc70e73864fbfb0b9f0cc56d5a5b26802c4026
3805c <carl.peyron@skola.taby.se>
2022-04-06 21:00:51 +0200

    version 1

--- script.js
+++ script.js
@@ -19,7 +19,87 @@ console.log(
 Höjd på canvas: ${canvas.height}`
 );
 
-function drawPicture() {
-  // Här skriver du funktionen som ritar bilden
+
+
+function drawTreeTop() {
+  const canvas = document.getElementById('canvas');
+  const c = canvas.getContext('2d');
+
+  c.beginPath();
+  c.ellipse(midX, midY, canvas.width/20, canvas.height/3, 0, Math.PI/2+0.5, Math.PI/2-0.5);
+  c.stroke();
+  c.fillStyle = "green";
+  c.fill();
+  }
+
+
+function drawTreeStem() {
+  var c = document.getElementById("canvas");
+  var c = c.getContext("2d");
+
+  c.beginPath();
+  c.lineWidth = "3";
+  c.strokeStyle = "black";
+  c.rect(midX-canvas.width/60, midY+canvas.height/3.45, canvas.width/30, canvas.height/3);
+  c.stroke();
+  c.fillStyle = "brown";
+  c.fill();
+  }
+
+
+function drawLeftHouse() {
+  var c = document.getElementById("canvas");
+  var c = c.getContext("2d");
+
+  c.beginPath();
+  c.lineWidth = "1";
+  c.strokeStyle = "black";
+  c.rect(midX-canvas.width/2.15, midY+canvas.height/3.8, canvas.width/4, canvas.height/4.3);
+  c.stroke();
+  }
+
+
+function drawRightHouse() {
+  var c = document.getElementById("canvas");
+  var c = c.getContext("2d");
+ 
+  c.beginPath();
+  c.lineWidth = "1";
+  c.strokeStyle = "black";
+  c.rect(midX+canvas.width/2.15-canvas.width/4, midY+canvas.height/3.8, canvas.width/4, canvas.height/4.3);
+  c.stroke();
+  }  
+
+
+function drawLeftRoof() {
+c.beginPath();
+c.moveTo(midX-canvas.width/2.15, midY+canvas.height/3.8);
+c.lineTo(midX-canvas.width/2.15+canvas.width/4, midY+canvas.height/3.8);
+c.lineTo(midX-canvas.width/2.15+canvas.width/8, midY);
+c.closePath();
+
+c.lineWidth = 1;
+c.strokeStyle = 'black';
+c.stroke();
 }
-drawPicture();
+
+
+function drawRightRoof() {
+ c.beginPath();
+ c.moveTo(midX+canvas.width/2.15-canvas.width/4, midY+canvas.height/3.8);
+ c.lineTo(midX+canvas.width/2.15, midY+canvas.height/3.8);
+ c.lineTo(midX+canvas.width/2.15-canvas.width/8, midY);
+ c.closePath();
+ 
+ c.lineWidth = 1;
+ c.strokeStyle = 'black';
+ c.stroke();
+ }
+
+
+drawTreeTop();
+drawTreeStem();
+drawLeftHouse();
+drawRightHouse();
+drawLeftRoof();
+drawRightRoof();
